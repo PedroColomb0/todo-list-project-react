3ce9db67a06e6e2bee1ab3454451d5b8b9bc7623
PedroColomb0 <pedrocolombodev@gmail.com>
2025-12-03 10:13:02 -0400

    separando em component filho e mae (Todo) e enviando os props da mae que seria nosso objeto da array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import "./App.css"
+import Todo from './components/Todo'
 
 function App(){
   const [todos,setTodos] = useState([
@@ -27,16 +28,7 @@ function App(){
     <h1>Lista de Tarefas</h1>
     <div className="todo-list">
       {todos.map((todo) => (
-        <div className="todo">
-          <div className='content'>
-            <p> {todo.text}</p>
-            <p className='category'> ({todo.category})</p>
-          </div>
-          <div>
-            <button>Completar</button>
-            <button>x</button>
-          </div>
-        </div>
+       <Todo todo={todo}/>
       ))}
     </div>
   </div>
